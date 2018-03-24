b37e55c189e00348ea9eb2101e9a8b87f1231621
David Lawanson <davida.lawanson@Davids-MacBook-Pro.local>
2018-03-23 21:55:54 -0500

    Set Background Color on cells & Built functionality for "Colors with Navigation" Challenge

---
+++ Colors With Navigation/Colors With Navigation/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors With Navigation
+//
+//  Created by David Lawanson on 3/23/18.
+//  Copyright © 2018 David Lawanson. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
